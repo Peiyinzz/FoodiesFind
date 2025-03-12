57b011fa34a694296e3332be90c21e3adce1b006
Peiyinzz <117030605+Peiyinzz@users.noreply.github.com>
2025-03-13 01:47:54 +0800

    Initialise firebase and separate screen files

---
+++ frontend/lib/firebase_options.dart
@@ -0,0 +1,63 @@
+// lib/firebase_options.dart
+
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, TargetPlatform, kIsWeb;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        return macos;
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyA2E0OZSVn_CiqI1j7-HtbOJVq6J1LCRow',
+    appId: '1:57555916564:android:e0ef840c16b9ec9e145401',
+    messagingSenderId: '57555916564',
+    projectId: 'foodiesfind-21552',
+    storageBucket: 'foodiesfind-21552.firebasestorage.app',
+  );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'your-api-key',
+    authDomain: 'your-auth-domain',
+    projectId: 'your-project-id',
+    storageBucket: 'your-storage-bucket',
+    messagingSenderId: 'your-sender-id',
+    appId: 'your-app-id',
+    measurementId: 'your-measurement-id',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'your-api-key',
+    appId: 'your-app-id',
+    messagingSenderId: 'your-sender-id',
+    projectId: 'your-project-id',
+    storageBucket: 'your-storage-bucket',
+    iosClientId: 'your-ios-client-id',
+    iosBundleId: 'com.example.frontend',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'your-api-key',
+    appId: 'your-app-id',
+    messagingSenderId: 'your-sender-id',
+    projectId: 'your-project-id',
+    storageBucket: 'your-storage-bucket',
+    iosClientId: 'your-ios-client-id',
+    iosBundleId: 'com.example.frontend',
+  );
+}
