c6f6d5803a4c63dc3bde4e4275efcc9b19029fee
Peiyinzz <117030605+Peiyinzz@users.noreply.github.com>
2025-05-23 03:58:12 +0800

    key value pairs in railway

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,25 @@ from fastapi import FastAPI
 from firebase_admin import credentials, firestore
 import firebase_admin
 from recommend import router as recommend_router
+import os
+import json
 
 # Initialize Firebase Admin
-cred = credentials.Certificate("firebase_config.json")  # Replace with your actual service account key file path
+#cred = credentials.Certificate("firebase_config.json")  # Replace with your actual service account key file path
+
+firebase_creds = {
+    "type": "service_account",
+    "project_id": os.environ["FIREBASE_PROJECT_ID"],
+    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": os.environ["FIREBASE_PRIVATE_KEY"],
+    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": os.environ["FIREBASE_CLIENT_ID"],
+    "auth_uri": os.environ["FIREBASE_AUTH_URI"],
+    "token_uri": os.environ["FIREBASE_TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["FIREBASE_AUTH_PROVIDER_CERT_URL"],
+    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_CERT_URL"]
+}
+cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
