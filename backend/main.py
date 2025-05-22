27ec067ea8a0364d8346b572c324b3410ae0bae2
Peiyinzz <117030605+Peiyinzz@users.noreply.github.com>
2025-05-23 03:12:20 +0800

    port

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,3 @@ async def root():
 
 # Include modular recommendation router
 app.include_router(recommend_router)
-
-# Required to run app on Railway
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
