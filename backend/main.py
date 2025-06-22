4cb0cc687b7772f1c6bee30f232304f1bae97c9f
Peiyinzz <117030605+Peiyinzz@users.noreply.github.com>
2025-06-22 08:17:57 +0800

    try remove replace

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ cred_dict = {
     "type": "service_account",
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
